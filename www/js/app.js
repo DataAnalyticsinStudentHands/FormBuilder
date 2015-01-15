31d9bfd0e37b9a2b6f07ebeedda72fafe9d07e49
CarlSteven <carl_steven@live.com>
2015-01-15 09:19:23 -0600

    No errors in the console, atleast.

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@
 
 /* App Module */
 
-var databaseModule = angular.module('databaseModule', ['restangular', 'databaseControllerModule', 'databaseServicesModule',  'ui.router']);
+var databaseModule = angular.module('databaseModule', ['restangular', 'databaseControllerModule', 'databaseServicesModule',  'ui.router', 'builder', 'builder.components', 'validator.rules']);
 
 databaseModule.config(
   function($stateProvider, $urlRouterProvider) {
@@ -40,7 +40,7 @@ databaseModule.config(
       });
   });
 
-databaseModule.run(['Restangular', '$rootScope', 'Auth', '$q', '$state', function(Restangular, $rootScope, Auth, $q, $state) {
+databaseModule.run(['Restangular', '$rootScope', 'Auth', '$q', '$state', '$builder', function(Restangular, $rootScope, Auth, $q, $state, $builder) {
 //    Restangular.setBaseUrl("http://localhost:8080/RESTFUL-WS/services/");
     Restangular.setBaseUrl("https://www.housuggest.org:8443/FormBuilder/");
     $rootScope.Restangular = function() {
@@ -60,4 +60,39 @@ databaseModule.run(['Restangular', '$rootScope', 'Auth', '$q', '$state', functio
         event.preventDefault(); 
       } else console.log("authed");
     });
+    
+        
+      $builder.registerComponent('sampleInput', {
+        group: 'from html',
+        label: 'Sample',
+        description: 'From html template',
+        placeholder: 'placeholder',
+        required: false,
+        validationOptions: [
+          {
+            label: 'none',
+            rule: '/.*/'
+          }, {
+            label: 'number',
+            rule: '[number]'
+          }, {
+            label: 'email',
+            rule: '[email]'
+          }, {
+            label: 'url',
+            rule: '[url]'
+          }
+        ],
+        templateUrl: 'partials/template.html',
+        popoverTemplateUrl: 'partials/popoverTemplate.html'
+      });
+    
+      return $builder.registerComponent('name', {
+        group: 'Default',
+        label: 'Name',
+        required: false,
+        arrayToText: true,
+        template: "<div class=\"form-group\">\n    <label for=\"{{formName+index}}\" class=\"col-md-4 control-label\" ng-class=\"{'fb-required':required}\">{{label}}</label>\n    <div class=\"col-md-8\">\n        <input type='hidden' ng-model=\"inputText\" validator-required=\"{{required}}\" validator-group=\"{{formName}}\"/>\n        <div class=\"col-sm-6\" style=\"padding-left: 0;\">\n            <input type=\"text\"\n                ng-model=\"inputArray[0]\"\n                class=\"form-control\" id=\"{{formName+index}}-0\"/>\n            <p class='help-block'>First name</p>\n        </div>\n        <div class=\"col-sm-6\" style=\"padding-left: 0;\">\n            <input type=\"text\"\n                ng-model=\"inputArray[1]\"\n                class=\"form-control\" id=\"{{formName+index}}-1\"/>\n            <p class='help-block'>Last name</p>\n        </div>\n    </div>\n</div>",
+        popoverTemplate: "<form>\n    <div class=\"form-group\">\n        <label class='control-label'>Label</label>\n        <input type='text' ng-model=\"label\" validator=\"[required]\" class='form-control'/>\n    </div>\n    <div class=\"checkbox\">\n        <label>\n            <input type='checkbox' ng-model=\"required\" />\n            Required\n        </label>\n    </div>\n\n    <hr/>\n    <div class='form-group'>\n        <input type='submit' ng-click=\"popover.save($event)\" class='btn btn-primary' value='Save'/>\n        <input type='button' ng-click=\"popover.cancel($event)\" class='btn btn-default' value='Cancel'/>\n        <input type='button' ng-click=\"popover.remove($event)\" class='btn btn-danger' value='Delete'/>\n    </div>\n</form>"
+      });
 }]);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,3 +70,40 @@ databaseController.controller('secureCtrl', ['$scope', 'Auth', '$state',
           $state.go('secure',{},{reload: true});
       }
   }]);
+
+databaseController.controller('DemoController', [
+    '$scope', '$builder', '$validator', function($scope, $builder, $validator) {
+      var checkbox, textbox;
+      textbox = $builder.addFormObject('default', {
+        id: 'textbox',
+        component: 'textInput',
+        label: 'Name',
+        description: 'Your name',
+        placeholder: 'Your name',
+        required: true,
+        editable: false
+      });
+      checkbox = $builder.addFormObject('default', {
+        id: 'checkbox',
+        component: 'checkbox',
+        label: 'Pets',
+        description: 'Do you have any pets?',
+        options: ['Dog', 'Cat']
+      });
+      $builder.addFormObject('default', {
+        component: 'sampleInput'
+      });
+      $scope.form = $builder.forms['default'];
+      $scope.input = [];
+      $scope.defaultValue = {};
+      $scope.defaultValue[textbox.id] = 'default value';
+      $scope.defaultValue[checkbox.id] = [true, true];
+      return $scope.submit = function() {
+        return $validator.validate($scope, 'default').success(function() {
+          return console.log('success');
+        }).error(function() {
+          return console.log('error');
+        });
+      };
+    }
+  ]);
