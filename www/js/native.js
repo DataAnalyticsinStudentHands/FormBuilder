1ae08b70269ba6e7a0447168f27d8b75e61fd1a0
nqv1994 <nqv1994@gmail.com>
2015-03-02 11:13:00 -0600

    Fix scrolling in Formbuilder

--- www/js/native.js
+++ www/js/native.js
@@ -3,16 +3,17 @@
  */
 
 $(window).scroll(function () {
-    // $('.grid').css('height', 600+'px');
     var fb = $('.formbuilderRight');
+    var checkhi=$( window ).height()-285;
     if ($(window).scrollTop() > 220) {
         fb.addClass('form-fixed');
-        $(".formbuilderRight .form-horizontal").css('height', $( window ).height()-145);
         $('.rmargin').addClass('form-title-fixed');
         $('.formbuilder').css('margin-top', 70);
     } else {
         fb.removeClass('form-fixed');
-        $(".formbuilderRight .form-horizontal").css('height', $( window ).height()-285+$(window).scrollTop());
+        if ($('.formbuilderLeft').height()>=$('.formbuilderRight').height()) {
+            $(".formbuilderRight .form-horizontal").css('height', $( window ).height()-285+$(window).scrollTop());
+        }
         $('.formbuilder').css('margin-top', '');
         $('.rmargin').removeClass('form-title-fixed');
     };
