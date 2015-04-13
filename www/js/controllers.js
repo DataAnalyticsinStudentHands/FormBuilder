dd5061c581b43363e8b91a5184744e77f75d025b
CarlSteven <carl_steven@live.com>
2015-04-13 09:48:15 -0500

    Update a bunch of stuff.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -354,6 +354,8 @@ formBuilderController.controller('builderCtrl', ['$scope', '$builder', '$validat
 formBuilderController.controller('formCtrl', ['$scope', '$builder', '$validator', '$stateParams', 'form', '$filter', 'responseService', '$state', 'ngNotify',
     function($scope, $builder, $validator, $stateParams, form, $filter, responseService, $state, ngNotify) {
         $scope.id = $stateParams.id;
+        $scope.receipt_required = form.send_receipt;
+        $scope.send_receipt = form.send_receipt;
         $scope.$parent.form_obj = form;
         $builder.forms[$scope.id] = null;
         form.questions.forEach(function(question){
@@ -374,18 +376,22 @@ formBuilderController.controller('formCtrl', ['$scope', '$builder', '$validator'
         $scope.form = $builder.forms[$scope.id];
         $scope.input = [];
         $scope.submit = function() {
-            $validator.validate($scope, $scope.id).success(function() {
-                responseService.newResponse($scope.input, $scope.id, $scope.uid, $scope.responder_email).then(function(){
-                    ngNotify.set("Form submission success!", "success");
-                    $state.go("finished", {"id": $scope.form_obj.id});
-                    $scope.input = null;
-                }, function(){
-                    ngNotify.set("Submission failed!", "error");
+            if ($scope.send_receipt && !$scope.responder_email) {
+                ngNotify.set("E-Mail is required to receive receipt.", "error");
+            } else {
+                $validator.validate($scope, $scope.id).success(function () {
+                    responseService.newResponse($scope.input, $scope.id, $scope.uid, $scope.responder_email).then(function () {
+                        ngNotify.set("Form submission success!", "success");
+                        $state.go("finished", {"id": $scope.form_obj.id});
+                        $scope.input = null;
+                    }, function () {
+                        ngNotify.set("Submission failed!", "error");
+                    });
+                }).error(function () {
+                    ngNotify.set("Form submission error, please verify form contents.", "error");
                 });
-            }).error(function() {
-                ngNotify.set("Form submission error, please verify form contents.", "error");
-            });
-        };
+            }
+        }
     }]);
 
 formBuilderController.controller('uploadCtrl',
