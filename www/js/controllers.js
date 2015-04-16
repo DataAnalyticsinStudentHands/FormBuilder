04e2899b9584725ff40b19ac1923fa262b5de8fc
Brown, Timothy A <t-a-brown@hotmail.com>
2015-04-16 14:02:55 -0500

    Filter now searches by title, subtitle, question label, and description.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -77,6 +77,42 @@ formBuilderController.controller('homeCtrl', ['$scope', 'Auth', '$state', 'formS
     function($scope, Auth, $state, formService, ngNotify, forms) {
         $scope.state = $state;
         $scope.myForms = forms;
+
+        $scope.filterByQuestion = function(inForm){
+
+            if($scope.query){
+
+                // Filters by name
+                var matchArr = inForm.name.match($scope.query);
+                if(matchArr && (matchArr.length > 0))
+                    return true;
+
+                // Filters by subtitle
+                if(inForm.subtitle)
+                    matchArr = inForm.subtitle.match($scope.query);
+                if(matchArr && (matchArr.length > 0))
+                    return true;
+
+                for(var i=0;i<inForm.questions.length;i++){
+
+                    // Filters by question label
+                    if(inForm.questions[i].label)
+                        matchArr = inForm.questions[i].label.match($scope.query);
+                    if(matchArr && (matchArr.length > 0))
+                        return true;
+
+                    // Filters by question description
+                    if(inForm.questions[i].description)
+                        matchArr = inForm.questions[i].description.match($scope.query);
+                    if(matchArr && (matchArr.length > 0))
+                        return true;
+                }
+
+                return false;
+            }
+            else
+                return true;
+        }
     }]);
 
 formBuilderController.controller('menuCtrl', ['$scope', 'Auth', 'ngNotify', '$state',
