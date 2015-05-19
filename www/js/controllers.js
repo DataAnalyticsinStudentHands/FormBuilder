58353c358d10336af97cf259b1a15c80e48a7106
CarlSteven <carl_steven@live.com>
2015-05-19 12:52:19 -0500

    luuguhijo

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -345,8 +345,7 @@ formBuilderController.controller('studiesCtrl', ['$scope', 'Auth', '$state', 'fo
     function($scope, Auth, $state, formService, responseService, $stateParams, ngNotify, form) {
         $scope.id = $stateParams.id;
         $scope.form = form;
-
-        $scope.studies = [{participants: ["bob", "hank"]}, {participants: ["tom", "tim"]}];
+        $scope.studies = [];
     }]);
 
 formBuilderController.controller('builderCtrl', ['$scope', '$builder', '$validator', 'formService', '$stateParams', '$filter', '$state', 'ngNotify', 'form',
