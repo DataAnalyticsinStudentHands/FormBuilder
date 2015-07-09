11ae92af09c1c5fd88e729eddc935d9800add75f
Binh Tran <o.otuto@gmail.com>
2015-07-09 16:26:16 -0600

    Added new directive that helps with ng-repeat. #136

--- www/js/directive.js
+++ www/js/directive.js
@@ -17,3 +17,19 @@ databaseModule.directive('title', ['$rootScope', '$timeout',
         };
     }
 ]);
+
+databaseModule.module('siApp.services').directive('postRepeatDirective', 
+  ['$timeout', '$log',  'TimeTracker', 
+  function($timeout, $log, TimeTracker) {
+    return function(scope, element, attrs) {
+      if (scope.$last){
+         $timeout(function(){
+             var timeFinishedLoadingList = TimeTracker.reviewListLoaded();
+             var ref = new Date(timeFinishedLoadingList);
+             var end = new Date();
+             $log.debug("## DOM rendering list took: " + (end - ref) + " ms");
+         });
+       }
+    };
+  }
+]);
